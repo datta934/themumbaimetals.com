215d89e0b4dfb17509dcea1cbf79d4e18cf61876
mauaa <mauaa@koder.com>
2020-06-28 11:48:27 +0530

    Added about us, products, mail feature

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -3,9 +3,6 @@ $(function () {
         "#contactForm input,#contactForm textarea,#contactForm button"
     ).jqBootstrapValidation({
         preventSubmit: true,
-        submitError: function ($form, event, errors) {
-            // additional error messages or events
-        },
         submitSuccess: function ($form, event) {
             event.preventDefault(); // prevent default submit behaviour
             // get values from FORM
@@ -18,58 +15,8 @@ $(function () {
             if (firstName.indexOf(" ") >= 0) {
                 firstName = name.split(" ").slice(0, -1).join(" ");
             }
-            $this = $("#sendMessageButton");
-            $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
-            $.ajax({
-                url: "/assets/mail/contact_me.php",
-                type: "POST",
-                data: {
-                    name: name,
-                    phone: phone,
-                    email: email,
-                    message: message,
-                },
-                cache: false,
-                success: function () {
-                    // Success message
-                    $("#success").html("<div class='alert alert-success'>");
-                    $("#success > .alert-success")
-                        .html(
-                            "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
-                        )
-                        .append("</button>");
-                    $("#success > .alert-success").append(
-                        "<strong>Your message has been sent. </strong>"
-                    );
-                    $("#success > .alert-success").append("</div>");
-                    //clear all fields
-                    $("#contactForm").trigger("reset");
-                },
-                error: function () {
-                    // Fail message
-                    $("#success").html("<div class='alert alert-danger'>");
-                    $("#success > .alert-danger")
-                        .html(
-                            "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
-                        )
-                        .append("</button>");
-                    $("#success > .alert-danger").append(
-                        $("<strong>").text(
-                            "Sorry " +
-                                firstName +
-                                ", it seems that my mail server is not responding. Please try again later!"
-                        )
-                    );
-                    $("#success > .alert-danger").append("</div>");
-                    //clear all fields
-                    $("#contactForm").trigger("reset");
-                },
-                complete: function () {
-                    setTimeout(function () {
-                        $this.prop("disabled", false); // Re-enable submit button when AJAX call is complete
-                    }, 1000);
-                },
-            });
+            $this = $("#sendMessageButton");// Disable submit button until AJAX call is complete to prevent duplicate messages
+
         },
         filter: function () {
             return $(this).is(":visible");
@@ -86,3 +33,16 @@ $(function () {
 $("#name").focus(function () {
     $("#success").html("");
 });
+
+
+function onMailSend() {
+    var name = document.getElementById("name").value;
+    var emailId = document.getElementById("email").value;
+    var phone = document.getElementById("phone").value;
+    var message = document.getElementById("message").value;
+    if (name === '' || emailId === '' || phone === '' || message === '') {
+        alert('Please fill all details in the form');
+    } else {
+        window.location.href = "mailto:sthemumbaimetals@gmail.com?subject=MUMBAI METALS ENQUIRY&body=Name: " + name + "%0DEmail: " + emailId + "%0DPhone: " + phone + "%0DMessage: " + message;
+    }
+}
